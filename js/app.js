62539e36b86488a6305f442e715057d3d91702b4
Chris Houdlette <choudlette@gmail.com>
2016-11-15 13:07:34 -0700

    working on weather display

--- js/app.js
+++ js/app.js
@@ -1,21 +1,31 @@
 $(document).ready(function(){
+/* Setting Global Variables */
   latitude = null;
   longitude = null;
   weatherdata = null;
+  icon = null;
+  skycons = null;
+  rand = null;
+  xml = null;
+  var divClone = $(".weathercontent").clone();
+/* Event Listeners */
   $('.searchbutton').click(findLatLong);
   $('.locationlist > ul').click(filterList);
+
+/* Functions */
 
   function findLatLong(event) {
     event.preventDefault();
     locationdata = $('.search').val();
-    $.get("https://maps.googleapis.com/maps/api/geocode/json?address="+ locationdata +"&key=", function(data){
-      console.log(data);
+    $.get("https://maps.googleapis.com/maps/api/geocode/json?address="+ locationdata +"&key=AIzaSyBnZE4qJU7UOsB-8_nYTpkJfKOjuCqOm8s", function(data){
       if (data.status ==="ZERO_RESULTS") {
         Materialize.toast('No Results Please Search Again', 4000);
       } else if (data.results.length === 1) {
         latitude = data.results[0].geometry.location.lat;
         longitude = data.results[0].geometry.location.lng;
         $('.weatherlocation').text(data.results[0].formatted_address);
+
+
       } else if (data.results.length > 1) {
         $('.locationlist').css('display','inline');
         for (i=0; i <data.results.length; i++) {
@@ -34,6 +44,7 @@ function filterList(event) {
   $('.search').val(event.target.innerText);
   $('.locationlist > ul').empty();
   $('.locationlist').css('display', 'none');
+
 }
 
 function createWeather(latitude, longitude) {
@@ -41,14 +52,80 @@ function createWeather(latitude, longitude) {
   url: 'https://api.darksky.net/forecast/fca786bd2d0d60b933581d4c8abe3203/'+latitude+','+longitude,
   data: "data",
   success: function(data) {
-
-    $('.temperature').append(' '+data.currently.temperature+ "F");
-    $('.currentconditions').append(' '+data.currently.summary);
-    $('.daily').append(' '+data.hourly.summary);
+    changeWeatherFields(data);
+    pickWord(data.currently.icon);
+    selectAndDisplayPoem(rand);
   },
   dataType: "jsonp"
 });
 
 }
+
+function createicon() {
+  $('#icon1').css('display', 'block');
+  skycons = new Skycons({"color": "black"});
+  console.log(icon);
+  skycons.add("icon1", Skycons.WIND);
+  skycons.play();
+}
+
+function changeWeatherFields(data) {
+  console.log(data);
+  $('.weathercontent').replaceWith(divClone.clone());
+  $('.temperature').append(' '+data.currently.temperature+ "F");
+  $('.currentconditions').append(' '+data.currently.summary);
+  $('.daily').append(' '+data.hourly.summary);
+  $('.precipprob').append(' '+ data.currently.precipProbability +'%');
+  $('.time').append(' '+ data.currently.time);
+  createicon();
+}
+
+/* Poem Arrays */
+function pickWord(currentWeather){
+  currentWeather = currentWeather.toString();
+  switch(currentWeather) {
+    case "fog":
+        array = ["mist", "fog", "murk", "haze"];
+        break;
+    case "clear-day":
+        array = ["sun", "day", "clear"];
+        break;
+    case "clear-night":
+        array = ["clear", "night", "dark", "moon", "star"];
+        break;
+    case "partly-cloudy-day":
+        array = ["cloud", "day", "overcast", "misty"];
+        break;
+    case "partly-cloudy-night":
+        array = ["cloudy", "night", "dark", "overcast", "misty", "evening"];
+        break;
+    case "cloudy":
+        array = ["cloud", "overcast", "misty", "cloudy"];
+        break;
+    case "rain":
+        array = ["rain", "raining", "drizzle", "pouring", "misty"];
+        break;
+    case "sleet":
+        array = ["sleet", "ice", "cold", "wet"];
+        break;
+    case "snow":
+        array = ["snow", "snowing", "misty"];
+        break;
+    case "wind":
+        array = ["wind", "windy", "breeze", "hurricane"];
+        break;
+  }
+    rand = array[Math.floor(Math.random() * array.length)];
+}
+function selectAndDisplayPoem(word) {
+  $.get('http://www.stands4.com/services/v2/poetry.php?uid=5425&tokenid= yz0jaxfwyLNjBzos&term=' + word, function(data){
+    xml = data;
+    resultarray = xml.getElementsByTagName('result');
+    selectedPoem = resultarray[Math.floor(Math.random() * resultarray.length)];
+    console.log(selectedPoem);
+
+  });
+}
+
 
 });
